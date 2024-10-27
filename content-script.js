393075f541240509c6ef8a2a23a9ca410858dbe1
AaronFish007 <aaron7yu@163.com>
2024-10-27 16:18:52 +0800

    feat: doves-docx v1.0.0.

---
+++ content-script.js
@@ -0,0 +1,54 @@
+﻿import mammoth from 'mammoth';
+import { load } from 'cheerio';
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "fromPopup") {
+        var data = Uint8Array.from(atob(request.data), c => c.charCodeAt(0)).buffer; //将base64转化回arrayBuffer
+        fillForm(data);		
+        sendResponse({response: "Function called"});
+    }
+});
+
+const fillForm = async(data) => {
+    mammoth.convertToHtml({arrayBuffer: data})
+    .then(function(result){
+        var html = result.value; // The generated HTML
+        var messages = result.messages; // Any messages, such as warnings during conversion
+        fill(html);
+        if(messages.length > 0) {
+            console.log(messages)
+        }
+    })
+    .catch(function(error) {
+        console.error(error);
+    });
+}
+
+const fill = async(html) => {
+    //get the target data from docx file
+    const $ = load(html);
+    const paragraphsText = [];
+    $('li').each((index, element) => {
+        paragraphsText.push($(element).text().trim());
+    });
+    
+    //create the target html input elements array. note that the order should be consistent with paragraphsText.
+    const inputId = [
+        "text-1",
+        "text-2",
+        "text-3",
+        "text-4",
+        "text-5"
+    ];
+
+    if(inputId.length === paragraphsText.length) {
+        for(var i = 0; i < inputId.length; i++) {
+            console.log(inputId[i], paragraphsText[i])
+            let input = document.getElementById(inputId[i]);
+            input.value = paragraphsText[i].split("：")[1];
+        }
+        alert('auto fill successfully!')
+    } else {
+        alert('input length error!')
+    }
+}
---
+++ popup.js
@@ -0,0 +1,29 @@
+﻿var base64String = ''
+
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    var files = event.target.files;
+    if (files.length > 0) {
+        var file = files[0];
+        var reader = new FileReader();
+        reader.onload = function(e) {
+        var arrayBuffer = e.target.result;
+        base64String = btoa(String.fromCharCode.apply(null, new Uint8Array(arrayBuffer)));
+        };
+        reader.readAsArrayBuffer(file);
+    }
+  });
+
+  document.getElementById('ok').addEventListener('click', () => {
+    if(base64String.length > 0) {
+        // 发送文件数据到 content script
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                action: "fromPopup",
+                data: base64String
+            }, function(response) {
+                // 可以在这里处理 content script 的响应
+                console.log('Received response from content script:', response);
+            });
+        });
+    }
+  })
